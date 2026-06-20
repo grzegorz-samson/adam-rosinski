beadff702454c865cd7f3f87c19475e017c0f16d
s20796 <s20796@pjwstk.edu.pl>
2026-06-21 00:28:51 +0200

    Add animated organization card backgrounds

---
+++ src/data/organizationAnimatedBackgrounds.ts
@@ -0,0 +1,18 @@
+const animatedMembershipBasePath = "/images/organizations/animated-memberships";
+
+export const organizationAnimatedBackgrounds: Record<string, string> = {
+  "acoustical-society-of-america": `${animatedMembershipBasePath}/acoustical-society-of-america.svg`,
+  "american-auditory-society": `${animatedMembershipBasePath}/american-auditory-society.svg`,
+  "association-for-computing-machinery": `${animatedMembershipBasePath}/association-for-computing-machinery.svg`,
+  "audio-engineering-society": `${animatedMembershipBasePath}/audio-engineering-society.svg`,
+  "cognitive-science-society": `${animatedMembershipBasePath}/cognitive-science-society.svg`,
+  "european-acoustics-association": `${animatedMembershipBasePath}/european-acoustics-association.svg`,
+  "fsnt-not": `${animatedMembershipBasePath}/fsnt-not.svg`,
+  ieee: `${animatedMembershipBasePath}/ieee.svg`,
+  "polish-acoustical-society": `${animatedMembershipBasePath}/polish-acoustical-society.svg`,
+  "polish-sound-engineers-association": `${animatedMembershipBasePath}/polish-association-of-sound-engineers.svg`,
+  "society-for-music-analysis": `${animatedMembershipBasePath}/society-for-music-analysis.svg`,
+  "society-for-music-perception-and-cognition": `${animatedMembershipBasePath}/society-for-music-perception-and-cognition.svg`,
+  "society-for-music-production-research": `${animatedMembershipBasePath}/society-for-music-production-research.svg`,
+  "society-for-music-theory": `${animatedMembershipBasePath}/society-for-music-theory.svg`,
+};
